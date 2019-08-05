77bc37a80d503021858b7852b93d5ce1f46071ab
Yun Xing <ink.xing.yun@gmail.com>
2019-08-05 15:23:23 -0400

    build russ page

--- components/players/russ.js
+++ components/players/russ.js
@@ -1,9 +1,16 @@
 import Layout from '../layout';
+import { useEffect } from 'react';
+import initWidgets from "../home/initWidgets";
+import $ from "jquery";
 
 const Russ = props => {
+    useEffect(() => {
+        initWidgets();
+    }, []);
+
   return (
     <Layout>
-      <div className="page">Russ</div>
+      <div className="page" style={{marginTop: 75}}>Russ</div>
     </Layout>
   );
 };
