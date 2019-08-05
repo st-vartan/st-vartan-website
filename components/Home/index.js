ccb5d51d51e990f5775647aec4c0013c631b8f40
Yun Xing <ink.xing.yun@gmail.com>
2019-08-05 14:56:38 -0400

    extract nav

--- components/Home/index.js
+++ components/Home/index.js
@@ -4,6 +4,7 @@ import Page from '../page';
 import Slider from 'react-slick';
 import { Link as ScrollLink } from 'react-scroll';
 import initWidgets from './initWidgets';
+import Nav from '../nav';
 
 const settings = {
     dots: true,
@@ -20,6 +21,7 @@ export default props => {
 
     return (
         <Page>
+            <Nav/>
             <div className="page" id="top">
                 <section
                     className="home-section bg-dark-alfa-30"
@@ -48,101 +50,7 @@ export default props => {
                         </ScrollLink>
                     </div>
                 </section>
-                <nav className="main-nav dark transparent stick-fixed">
-                    <div className="full-wrapper relative clearfix">
-                        <div className="nav-logo-wrap">
-                            <a href="intro.html" className="logo">
-                                <img src="/static/images/logo-white.png" alt="" />
-                            </a>
-                        </div>
 
-                        <div className="mobile-nav">
-                            <i className="fa fa-bars"></i>
-                        </div>
-
-                        <div className="inner-nav desktop-nav">
-                            <ul className="clearlist">
-                                <li>
-                                    <a href="#" className="mn-has-sub active">
-                                        Home <i className="fa fa-angle-down"></i>
-                                    </a>
-
-                                    <ul className="mn-sub mn-has-multi">
-                                        <li className="mn-sub-multi">
-                                            <a className="mn-group-title">Multi Page</a>
-
-                                            <ul>
-                                                <li>
-                                                    <a href="mp-index.html">Main Demo</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-
-                                <li>
-                                    <a href="#" className="mn-has-sub">
-                                        <i className="fa fa-search"></i> Search
-                                    </a>
-
-                                    <ul className="mn-sub">
-                                        <li>
-                                            <div className="mn-wrap">
-                                                <form method="post" className="form">
-                                                    <div className="search-wrap">
-                                                        <input
-                                                            type="text"
-                                                            className="form-control search-field"
-                                                            placeholder="Search..."
-                                                        />
-                                                        <button
-                                                            className="search-button animate"
-                                                            type="submit"
-                                                            title="Start Search"
-                                                        >
-                                                            <i className="fa fa-search"></i>
-                                                        </button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </li>
-                                {/* End Search */}
-
-                                {/* Cart */}
-                                <li>
-                                    <a href="#">
-                                        <i className="fa fa-shopping-cart"></i> Cart(0)
-                                    </a>
-                                </li>
-                                {/* End Cart */}
-
-                                {/* Languages */}
-                                <li>
-                                    <a href="#" className="mn-has-sub">
-                                        Eng <i className="fa fa-angle-down"></i>
-                                    </a>
-
-                                    <ul className="mn-sub">
-                                        <li>
-                                            <a href="">English</a>
-                                        </li>
-                                        <li>
-                                            <a href="">France</a>
-                                        </li>
-                                        <li>
-                                            <a href="">Germany</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                {/* End Languages */}
-                            </ul>
-                        </div>
-                        {/* End Main Menu */}
-                    </div>
-                </nav>
-                {/* End Navigation panel */}
 
                 {/* About Section */}
                 <section className="page-section" id="about">
--- components/nav.js
+++ components/nav.js
@@ -1,62 +1,102 @@
 import Head from './head';
 import Link from 'next/link';
-
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
+import Page from "./page";
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link prefetch href="/">
-          <a>Home</a>
-        </Link>
-        <Link prefetch href="/vis">
-          <a>Vis</a>
-        </Link>
-      </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </ul>
+    <nav className="main-nav dark transparent stick-fixed">
+      <div className="full-wrapper relative clearfix">
+        <div className="nav-logo-wrap">
+          <a href="intro.html" className="logo">
+            <img src="/static/images/logo-white.png" alt="" />
+          </a>
+        </div>
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
+        <div className="mobile-nav">
+          <i className="fa fa-bars"></i>
+        </div>
+
+        <div className="inner-nav desktop-nav">
+          <ul className="clearlist">
+            <li>
+              <a href="#" className="mn-has-sub active">
+                Home <i className="fa fa-angle-down"></i>
+              </a>
+
+              <ul className="mn-sub mn-has-multi">
+                <li className="mn-sub-multi">
+                  <a className="mn-group-title">Multi Page</a>
+
+                  <ul>
+                    <li>
+                      <a href="mp-index.html">Main Demo</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <a href="#" className="mn-has-sub">
+                <i className="fa fa-search"></i> Search
+              </a>
+
+              <ul className="mn-sub">
+                <li>
+                  <div className="mn-wrap">
+                    <form method="post" className="form">
+                      <div className="search-wrap">
+                        <input
+                            type="text"
+                            className="form-control search-field"
+                            placeholder="Search..."
+                        />
+                        <button
+                            className="search-button animate"
+                            type="submit"
+                            title="Start Search"
+                        >
+                          <i className="fa fa-search"></i>
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </li>
+              </ul>
+            </li>
+            {/* End Search */}
+
+            {/* Cart */}
+            <li>
+              <a href="#">
+                <i className="fa fa-shopping-cart"></i> Cart(0)
+              </a>
+            </li>
+            {/* End Cart */}
+
+            {/* Languages */}
+            <li>
+              <a href="#" className="mn-has-sub">
+                Eng <i className="fa fa-angle-down"></i>
+              </a>
+
+              <ul className="mn-sub">
+                <li>
+                  <a href="">English</a>
+                </li>
+                <li>
+                  <a href="">France</a>
+                </li>
+                <li>
+                  <a href="">Germany</a>
+                </li>
+              </ul>
+            </li>
+            {/* End Languages */}
+          </ul>
+        </div>
+        {/* End Main Menu */}
+      </div>
+    </nav>
 );
 
 export default Nav;
