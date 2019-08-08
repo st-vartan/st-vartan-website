7861a2a10b2dec396350f747f56674d9674ee366
Yun Xing <ink.xing.yun@gmail.com>
2019-08-07 22:56:22 -0400

    home page structure

--- components/home/index.js
+++ components/home/index.js
@@ -1,17 +1,8 @@
 import { useEffect } from 'react';
 
 import Layout from '../layout';
-import Slider from 'react-slick';
-import { Link as ScrollLink } from 'react-scroll';
 import initWidgets from './initWidgets';
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-};
 
 import MapSection from './sections/map';
 import RuleSection from './sections/rules';
@@ -22,6 +13,7 @@ import IntroDetailSection from './sections/intro-detail';
 import FooterSection from './sections/footer';
 import LatestGallerySection from './sections/latest-gallery';
 import TeamsSection from './sections/teams';
+import QuoteSection from './sections/quote';
 
 export default props => {
   useEffect(() => {
@@ -35,106 +27,15 @@ export default props => {
         {/*<IntroductionSection/>*/}
         <AboutSection />
         <hr className="mt-0 mb-0 " />
+        <RuleSection />
         <IntroDetailSection />
         <hr className="mt-0 mb-0" />
-        <LatestGallerySection/>
-
-        {/* Call Action Section */}
-
-        {/* End Call Action Section */}
-
-        {/* Testimonials Section */}
-        <section
-          className="page-section bg-dark bg-dark-alfa-90 fullwidth-slider"
-          data-background="/static/images/section-bg-3.jpg"
-        >
-          <Slider {...settings}>
-            {/* Slide Item */}
-            <div>
-              <div className="container relative">
-                <div className="row">
-                  <div className="col-md-8 col-md-offset-2 align-center">
-                    {/* Section Icon */}
-                    <div className="section-icon">
-                      <span className="icon-quote"></span>
-                    </div>
-                    {/* Section Title */}
-                    <h3 className="small-title font-alt">What people say?</h3>
-                    <blockquote className="testimonial white">
-                      <p>
-                        Phasellus luctus commodo ullamcorper a posuere rhoncus
-                        commodo elit. Aenean congue, risus utaliquam dapibus.
-                        Thanks!
-                      </p>
-                      <footer className="testimonial-author">
-                        John Doe, doodle inc.
-                      </footer>
-                    </blockquote>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* End Slide Item */}
-
-            {/* Slide Item */}
-            <div>
-              <div className="container relative">
-                <div className="row">
-                  <div className="col-md-8 col-md-offset-2 align-center">
-                    {/* Section Icon */}
-                    <div className="section-icon">
-                      <span className="icon-quote"></span>
-                    </div>
-                    {/* Section Title */}
-                    <h3 className="small-title font-alt">What people say?</h3>
-                    <blockquote className="testimonial white">
-                      <p>
-                        Phasellus luctus commodo ullamcorper a posuere rhoncus
-                        commodo elit. Aenean congue, risus utaliquam dapibus.
-                        Thanks!
-                      </p>
-                      <footer className="testimonial-author">
-                        John Doe, doodle inc.
-                      </footer>
-                    </blockquote>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* End Slide Item */}
-
-            {/* Slide Item */}
-            <div>
-              <div className="container relative">
-                <div className="row">
-                  <div className="col-md-8 col-md-offset-2 align-center">
-                    {/* Section Icon */}
-                    <div className="section-icon">
-                      <span className="icon-quote"></span>
-                    </div>
-                    {/* Section Title */}
-                    <h3 className="small-title font-alt">What people say?</h3>
-                    <blockquote className="testimonial white">
-                      <p>
-                        Phasellus luctus commodo ullamcorper a posuere rhoncus
-                        commodo elit. Aenean congue, risus utaliquam dapibus.
-                        Thanks!
-                      </p>
-                      <footer className="testimonial-author">
-                        John Doe, doodle inc.
-                      </footer>
-                    </blockquote>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* End Slide Item */}
-          </Slider>
-        </section>
-        <hr className="mt-0 mb-0 " />
-        <hr className="mt-0 mb-0 " />
         <TeamsSection/>
-        <RuleSection />
+        <LatestGallerySection/>
+        {/*<QuoteSection/>*/}
+        <hr className="mt-0 mb-0 " />
+
+
         <MapSection />
         <FooterSection />
       </div>
