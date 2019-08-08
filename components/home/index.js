23c29462757403f67fea2c5a1747157cd7b26941
Yun Xing <ink.xing.yun@gmail.com>
2019-08-07 22:01:13 -0400

    cleanup

--- components/home/index.js
+++ components/home/index.js
@@ -35,62 +35,6 @@ export default props => {
         <hr className="mt-0 mb-0 " />
         <IntroDetailSection />
 
-        {/* Process Section */}
-        <section className="page-section">
-          <div className="container relative">
-            {/* Features Grid */}
-            <div className="row alt-features-grid">
-              {/* Text Item */}
-              <div className="col-sm-3">
-                <div className="alt-features-item align-center">
-                  <div className="alt-features-descr align-left">
-                    <h4 className="mt-0 font-alt">Work process</h4>
-                    Lorem ipsum dolor sit amet, c-r adipiscing elit. In maximus
-                    ligula semper metus pellentesque mattis. Maecenas volutpat,
-                    diam enim.
-                  </div>
-                </div>
-              </div>
-              {/* End Text Item */}
-
-              {/* Features Item */}
-              <div className="col-sm-3">
-                <div className="alt-features-item align-center">
-                  <div className="alt-features-icon">
-                    <span className="icon-chat"></span>
-                  </div>
-                  <h3 className="alt-features-title font-alt">1. Discuss</h3>
-                </div>
-              </div>
-              {/* End Features Item */}
-
-              {/* Features Item */}
-              <div className="col-sm-3">
-                <div className="alt-features-item align-center">
-                  <div className="alt-features-icon">
-                    <span className="icon-browser"></span>
-                  </div>
-                  <h3 className="alt-features-title font-alt">2. Make</h3>
-                </div>
-              </div>
-              {/* End Features Item */}
-
-              {/* Features Item */}
-              <div className="col-sm-3">
-                <div className="alt-features-item align-center">
-                  <div className="alt-features-icon">
-                    <span className="icon-heart"></span>
-                  </div>
-                  <h3 className="alt-features-title font-alt">3. Product</h3>
-                </div>
-              </div>
-              {/* End Features Item */}
-            </div>
-            {/* End Features Grid */}
-          </div>
-        </section>
-        {/* End Process Section */}
-
         {/* Divider */}
         <hr className="mt-0 mb-0" />
         {/* End Divider */}
