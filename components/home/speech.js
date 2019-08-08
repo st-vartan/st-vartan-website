3c04050788316879c298d19278828aaaa7a4d789
Yun Xing <ink.xing.yun@gmail.com>
2019-08-07 22:29:06 -0400

    move full speech out

---
+++ components/home/speech.js
@@ -0,0 +1,62 @@
+const speech = () => {
+  return (
+    <section className="page-section">
+      <div className="container relative">
+        <div className="row">
+          <div className="col-md-5 col-lg-4 mb-sm-40">
+            {/* About Project */}
+            <div className="text">
+              <h3 className="font-alt mb-30 mb-xxs-10">Rules</h3>
+              <p>
+                But the height of the rim on the 35th street side has always
+                been 11 feet and the height of the of the 36 street side has
+                always been 10 1/2. And the out of bounds has always been the
+                poles
+              </p>
+              <p>
+                “The regulars like us “ use that to our advantage to beat any
+                non regular that come to the courts . It is our rules , just
+                like every court in New York City has their rules
+              </p>
+              <p>
+                So maybe we should just leave it how it is , I know some of you
+                go play elsewhere and it’s not the same. That’s why this our
+                court with our rules and maybe we should keep it that way . If
+                you can shoot on a 11 double foot rim than you can shoot
+                anywhere . What I would say is just leave it how it is , because
+                you guys are use to it already . Just protect the court when you
+                see an unfamiliar face , more importantly an unfamiliar 5 , and
+                just win .
+              </p>
+
+              <div className="mt-40">
+                <a
+                  href="http://themeforest.net/user/theme-guru/portfolio"
+                  className="btn btn-mod btn-border btn-round btn-medium"
+                  target="_blank"
+                >
+                  Buy Now
+                </a>
+              </div>
+            </div>
+            {/* End About Project */}
+          </div>
+
+          <div className="col-md-7 col-lg-offset-1">
+            {/* Work Gallery */}
+            <div className="work-full-media mt-0 white-shadow wow fadeInUp">
+              <ul className="clearlist work-full-slider owl-carousel">
+                <li>
+                  <img src="/static/images/full-project-4.jpg" alt="" />
+                </li>
+              </ul>
+            </div>
+            {/* End Work Gallery */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default speech;
