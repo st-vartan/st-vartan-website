9ec14cc82c1472717a2e6c73df277c5d33b79bd7
Yun Xing <ink.xing.yun@gmail.com>
2019-08-12 15:35:23 -0400

    fix style

--- components/home/sections/leagueStyle.js
+++ components/home/sections/leagueStyle.js
@@ -2,17 +2,17 @@ import Link from 'next/link';
 
 const leagueStyle = props => {
   return (
-    <section class="page-section pt-0 pb-0 banner-section bg-dark">
-      <div class="container relative">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="mt-140 mt-lg-80 mb-140 mb-lg-80">
-              <div class="banner-content">
-                <h3 class="banner-heading font-alt">Basketball Lifestyle</h3>
-                <div class="banner-decription">
+    <section className="page-section pt-0 pb-0 banner-section bg-dark">
+      <div className="container relative">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="mt-140 mt-lg-80 mb-140 mb-lg-80">
+              <div className="banner-content">
+                <h3 className="banner-heading font-alt">Basketball Lifestyle</h3>
+                <div className="banner-decription">
                   Pour your energy on court and earn your name at St.Vartan park
                 </div>
-                <div class="local-scroll">
+                <div className="local-scroll">
                   <Link href={'/lifestyle'}>
                     <a className="btn btn-mod btn-w btn-medium btn-round">
                       Explore more
@@ -23,7 +23,7 @@ const leagueStyle = props => {
             </div>
           </div>
 
-          <div class="col-sm-6">
+          <div className="col-sm-6">
             <img
               src="/static/images/lifestyle/tshirt_white_perfect_shot_carlos.png"
               alt=""
