4405ddf6602f2a915199aac08f56fd055ebd2cd3
Yun Xing <ink.xing.yun@gmail.com>
2019-08-13 15:09:35 -0400

    footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -14,37 +14,30 @@ const footer = () => (
         </ScrollLink>
       </div>
 
-      {/* End Footer Logo */}
+      {/*<div className="footer-social-links mb-110 mb-xs-60">*/}
+      {/*  <a href="#" title="Facebook" target="_blank">*/}
+      {/*    <i className="fa fa-facebook"></i>*/}
+      {/*  </a>*/}
+      {/*  <a href="#" title="Twitter" target="_blank">*/}
+      {/*    <i className="fa fa-twitter"></i>*/}
+      {/*  </a>*/}
+      {/*  <a href="#" title="Behance" target="_blank">*/}
+      {/*    <i className="fa fa-behance"></i>*/}
+      {/*  </a>*/}
+      {/*  <a href="#" title="LinkedIn+" target="_blank">*/}
+      {/*    <i className="fa fa-linkedin"></i>*/}
+      {/*  </a>*/}
+      {/*  <a href="#" title="Pinterest" target="_blank">*/}
+      {/*    <i className="fa fa-pinterest"></i>*/}
+      {/*  </a>*/}
+      {/*</div>*/}
 
-      {/* Social Links */}
-      <div className="footer-social-links mb-110 mb-xs-60">
-        <a href="#" title="Facebook" target="_blank">
-          <i className="fa fa-facebook"></i>
-        </a>
-        <a href="#" title="Twitter" target="_blank">
-          <i className="fa fa-twitter"></i>
-        </a>
-        <a href="#" title="Behance" target="_blank">
-          <i className="fa fa-behance"></i>
-        </a>
-        <a href="#" title="LinkedIn+" target="_blank">
-          <i className="fa fa-linkedin"></i>
-        </a>
-        <a href="#" title="Pinterest" target="_blank">
-          <i className="fa fa-pinterest"></i>
-        </a>
-      </div>
-      {/* End Social Links */}
-
-      {/* Footer Text */}
       <div className="footer-text">
         <div className="footer-copy font-alt">
           <a href="https://bigfatdog.github.io/" target="_blank">
             &copy; Yun Xing 2019
           </a>
         </div>
-
-        <div className="footer-made">Made with love for hoopers</div>
       </div>
     </div>
 
