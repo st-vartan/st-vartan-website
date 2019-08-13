dba822ee919106dd1c3190a8cb182a8ca5c3399b
Yun Xing <ink.xing.yun@gmail.com>
2019-08-13 14:36:02 -0400

    add players

--- components/players/data.js
+++ components/players/data.js
@@ -17,12 +17,23 @@ const data = [
     desc: 'King',
     url: 'russ',
   },
-  ,
   {
     image: '/static/images/players/370/george.jpg',
     name: 'George',
     desc: 'Big 3 from Downtown',
     url: 'george',
+  },
+  {
+    image: '/static/images/players/370/jon.jpg',
+    name: 'Jon',
+    desc: 'Lighten Spear',
+    url: 'jon',
+  },
+  {
+    image: '/static/images/players/370/joel.jpg',
+    name: 'Joel',
+    desc: 'J',
+    url: 'joel',
   },
 ];
 
