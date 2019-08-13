0547299351f6823f68c8bac261eff641bfcc72d1
Yun Xing <ink.xing.yun@gmail.com>
2019-08-13 15:22:46 -0400

    player data

--- components/players/data.js
+++ components/players/data.js
@@ -1,23 +1,5 @@
 const data = [
   {
-    image: '/static/images/players/370/yun.jpg',
-    name: 'Yun',
-    desc: 'Boardman',
-    url: 'yun',
-  },
-  {
-    image: '/static/images/players/370/sean.jpg',
-    name: 'Sean',
-    desc: 'Space Jam',
-    url: 'sean',
-  },
-  {
-    image: '/static/images/players/370/russ.jpg',
-    name: 'Russ',
-    desc: 'King',
-    url: 'russ',
-  },
-  {
     image: '/static/images/players/370/george.jpg',
     name: 'George',
     desc: 'Big 3 from Downtown',
@@ -34,6 +16,24 @@ const data = [
     name: 'Joel',
     desc: 'J',
     url: 'joel',
+  },
+  {
+    image: '/static/images/players/370/russ.jpg',
+    name: 'Russ',
+    desc: 'King',
+    url: 'russ',
+  },
+  {
+    image: '/static/images/players/370/sean.jpg',
+    name: 'Sean',
+    desc: 'Space Jam',
+    url: 'sean',
+  },
+  {
+    image: '/static/images/players/370/yun.jpg',
+    name: 'Yun',
+    desc: 'Boardman',
+    url: 'yun',
   },
 ];
 
